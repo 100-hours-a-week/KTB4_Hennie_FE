7c99ce1155ba49d404503e180146e847b5899066
iey704 <iey704@gachon.ac.kr>
2026-07-20 20:49:16 +0900

    style: Tailwind CSS 추가

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 import babelConfig from './babel.config.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel(babelConfig)],
+  plugins: [react(), babel(babelConfig), tailwindcss()],
   server: {
     proxy: {
       '/api': {
