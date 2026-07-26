43925251e27b0d5ff235cea0ad580f9ec62f27be
iey704 <iey704@gachon.ac.kr>
2026-07-26 22:18:02 +0900

    build: React Compiler 설정 제거 및 Node 버전 고정

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
-import babelConfig from './babel.config.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel(babelConfig), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
